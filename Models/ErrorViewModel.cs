c08fdf9044df28f1e188d4c82f56165b417cf563
HP <reshmaksriet@gmail.com>
2023-09-14 10:57:03 +0530

    adding all

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApUsers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApUsers
+{
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+
+        public string UserEmail { get; set; }
+
+
+    }
+}
